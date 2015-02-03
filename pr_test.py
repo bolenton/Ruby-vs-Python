d4ab390cb2c9ef1c7285dc1e811d07f390641ae9
Taurus Omejia <bolenton@gmail.com>
2015-02-03 17:20:16 -0600

    first commit
    very simple text based game, i wrote in ruby and python, to compare, as
    i learn both

---
+++ pr_test.py
@@ -0,0 +1,33 @@
+# Simple text based game, Try to guess the number in 10 times.
+
+from random import randint
+
+print("\n\nWelcome to the 'Random Number Game!' ")
+player = raw_input("What is your name? >>>  ")
+name = player.capitalize()
+
+print("\nHello {}!, I am thinking of a number between 1 - 100!").format(name)
+print("I will give you 10 tries to guess my number, ready? GO!")
+
+number = randint(1, 100)
+turns = 0
+
+while turns < 10:
+	guess = int(raw_input("\nWhat do you think my number is >>> "))
+	
+	if guess == number:
+		print("\nGood Job! You guessed it on turn {}!").format(turns)
+		print("\nGame Over, You Win!!!\n\n\n")
+		break
+	elif guess > number:
+		print("Sorry thats too high.")
+	elif guess < number:
+		print("Sorry thats too low.")
+
+	turns+= 1
+	total_turns = 10 - turns 
+	print("You have {} turns remaining.").format(total_turns)
+
+	if turns == 10:
+		print("\n\nMy number was {}.").format(number)
+		print("Game Over. You Lose.\n\n\n\n")
---
+++ rb_test.rb
@@ -0,0 +1,37 @@
+# Simple text based game, Try to guess the number in 10 times.
+
+puts "\n\nWelcome to the 'Random Number Game!' "
+print "What is your name? >>>  "
+player = gets
+name = player.upcase.chomp
+
+puts "\n\nHello #{name}!, I am thinking of a number between 1 - 100!"
+puts "I will give you 10 tries to guess my number, ready? GO!"
+
+number = rand(100) + 1
+turns = 0
+
+while turns < 10
+	print "\n\nWhat do you think my number is >>> "
+	guess = gets.to_i
+
+	if guess == number
+		puts "Good Job! You guessed it on turn #{turns}!"
+		puts "\nGame Over, You Win!!!\n\n\n"
+		break
+	elsif guess > number
+		puts "Sorry thats too high."
+	elsif guess < number
+		puts "Sorry thats too low"
+	end
+
+	turns = turns + 1
+	turns_left = 10 - turns
+	puts "You have #{turns_left} turns remaining."
+	
+	if turns == 10
+		puts "\n\nMy number was #{number}."
+		puts "Game Over. You Lose.\n\n\n\n"
+	end
+end
+ 
